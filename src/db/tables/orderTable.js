8daa272bd2b2e083170464a3ad8b17759f3f6b2c
Misha <shestopalkamishayt@gmail.com>
2025-06-17 18:48:45 +0300

    Create OrderTable

---
+++ src/db/tables/orderTable.js
@@ -0,0 +1,28 @@
+const { query } = require('../dbPg.js');
+
+const createOrderTable = `
+    CREATE TABLE IF NOT EXISTS orders (
+        id SERIAL PRIMARY KEY,
+        product_name VARCHAR(100) NOT NULL,
+        supplier_country VARCHAR(100) NOT NULL,
+        place_of_delivery VARCHAR(100) NOT NULL,
+        price_of_the_product INT NOT NULL,
+        quantity_of_goods INT NOT NULL,
+        compartment_name VARCHAR(100) NOT NULL,
+        arrived BOOLEAN DEFAULT FALSE,
+        status VARCHAR(100) NOT NULL
+    );
+`
+
+
+function initOrderTable() {
+    return query(createOrderTable)
+        .then(() => {
+            console.log("Table 'order' created!");
+        })
+        .catch((err) => {
+            console.error('Error creating table', err);
+        })
+}
+
+module.exports = { initOrderTable };
